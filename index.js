989c412df58e003ebd772bac13a43559d4517746
mumit-1 <mumitkhan85@gmail.com>
2025-06-29 02:29:47 +0600

    ok18

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 
+// const uri = `mongodb+srv://CoreUP:t48i0MPvfWFwP3Mw@cluster0.yrnn1sn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yrnn1sn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,8 +26,17 @@ async function run() {
   try {
     //  await client.connect();
     const toolsCollection = client.db("sportsDb").collection("tools");
-
-    
+    app.post("/addProduct",async(req,res)=>{
+      const newProduct = req.body;
+      console.log(newProduct);
+      const result = await toolsCollection.insertOne(newProduct);
+      res.send(result);
+    })
+    app.get('/addProduct',async(req,res)=>{
+      const cursor = toolsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
